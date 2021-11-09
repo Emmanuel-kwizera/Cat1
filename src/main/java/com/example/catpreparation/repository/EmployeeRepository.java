1c53f928604e6b61c8560be162fddea9e9ea43fe
Kwizera Emmanuel <51673007+Emmanuel-kwizera@users.noreply.github.com>
2021-11-09 11:03:29 +0200

    Delete src/main directory

--- src/main/java/com/example/catpreparation/repository/EmployeeRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.catpreparation.repository;
-
-import com.example.catpreparation.model.Employee;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface EmployeeRepository extends JpaRepository<Employee,Long> {
-}
